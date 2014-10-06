d37a2fe628db1a6948f93415fe2b1c93d9d672c3
Chris Opperwall <chriso@ifixit.com>
2014-10-06 02:16:23 -0400

    Add analytics script

--- index.php
+++ index.php
@@ -8,8 +8,6 @@
    <link rel='stylesheet' media='screen and (max-device-width: 768px)'/>
 </head>
 <body>
-   <?php include_once("../analyticstracking.php") ?>
-
    <!-- The Header should go here, code background, "The Blog" title -->
    <!-- NavBar is on the side? Either that or list of blog titles -->
    <header>
@@ -49,5 +47,15 @@
 
    <!-- Twitter API scripts -->
    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+   <script>
+     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+               m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+     ga('create', 'UA-46793736-1', 'auto');
+     ga('send', 'pageview');
+
+     </script>
 </body>
 </html>
